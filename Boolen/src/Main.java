ff5cd07e18d16df0df8ee9e2cb0343385f4a5a3c
Jonathan Engstrand <141049701+Engstrand701@users.noreply.github.com>
2024-10-19 13:31:16 +0100

    Have a look.
    Have a look over and give feedback.

---
+++ Boolen/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean isJAVAfun = true;
+        boolean isMoreVegetationBetter = false;
+        System.out.println(isJAVAfun);
+        System.out.println(isMoreVegetationBetter);
+    }
+}
